7298245deef1a86f94e59d5758ebd13f6254e7e1
Arthur P <arthur_p@instigatemobile.com>
2020-08-26 01:28:06 +0400

    updated algorithmz, made slight refactoring

--- Algorithmz/src/com/learn/im/dynamica/Algorithmz.kt
+++ Algorithmz/src/com/learn/im/dynamica/Algorithmz.kt
@@ -11,26 +11,30 @@ fun fibonacciOf(n: Int): Long {
     return f.last()
 }
 
-data class BackpackItem(val weight: Int, val value: Int, val id: Double = Math.random())
-
 // s = "" || s1 == "" -> ""
 // s.last = s1.last -> 1 + s.last + f(s - 1, s1 - 1)
 // if len s > s1 -> f(s - 1, s1) else -> f(s, s1 - 1)
-fun String.longestCommonSubsequence(string: String): Int {
-    val resultMatrix = Array(this.length + 1) {
-        IntArray(string.length + 1)
+fun String.longestCommonSubsequence(string: String): String {
+    val f = Array(this.length + 1) {
+        IntArray(string.length + 1) { 0 }
     }
     for (i in 1..this.length){
         for(j in 1..string.length){
             if(this[i - 1] == string[j - 1]){
-                resultMatrix[i][j] = 1 + resultMatrix[i - 1][j - 1]
+                f[i][j] = 1 + f[i - 1][j - 1]
             }
             else{
-                resultMatrix[i][j] = resultMatrix[i - 1][j].coerceAtLeast(resultMatrix[i][j - 1])
+                f[i][j] = f[i - 1][j].coerceAtLeast(f[i][j - 1])
             }
         }
     }
-    return resultMatrix.last().last()
+    var result = ""
+    for (i in 1 until f.last().size ){
+        if(f.last()[i] != f.last()[i - 1]){
+            result += string[i - 1]
+        }
+    }
+    return result
 }
 
 fun String.has(substring: String): Boolean {
@@ -52,30 +56,49 @@ fun String.has(substring: String): Boolean {
     return false
 }
 
+data class BackpackItem(val weight: Int, val value: Int, val id: Double = Math.random()){
+    override fun toString(): String = "[V(${value}) W(${weight})]"
+}
+
+
+// ~ f(l, w) = value + max(f([l - 1], w - w1, f([l - 1], w - w1) 2^n
+fun maxBackpackValue(items: List<BackpackItem>, maxWeight: Int): List<BackpackItem> {
+    val f = Array(maxWeight + 1) {
+        IntArray(items.size + 1)
+    }
+    for (i in 1..items.size){
+        val weight = items[i - 1].weight
+        val value = items[i - 1].value
+
+        for (j in 1 until weight) {
+            if(j < maxWeight){
+                f[j][i] = f[j][i - 1]
+            }
+        }
+        for (j in weight..maxWeight){
+            f[j][i] = f[j][i - 1].coerceAtLeast(value + f[j - weight][i - 1])
+        }
+    }
+
+    val listOfTaken = mutableListOf<BackpackItem>()
+    var remainingWeight = maxWeight
+    var currentValue = f.last().last()
+    for (i in items.size downTo 1){
+        if (currentValue != f[remainingWeight][i - 1]){
+            currentValue -= items[i-1].value
+            remainingWeight -= items[i-1].weight
+            listOfTaken.add(items[i-1])
+        }
+        if(remainingWeight < 0) break
+    }
+
+    return listOfTaken
+}
+
 fun String.hazZ(string: String):Boolean{
     TODO()
 }
 
 fun String.getZ(): IntArray {
     TODO()
-}
-
-fun maxBackpackValue(items: List<BackpackItem>, maxWeight: Int): Int {
-    val values = Array(maxWeight + 1) {
-        Array(items.size + 1) { 0 }
-    }
-    for (i in 1..items.size){
-        val weight = items[i - 1].weight
-        val value = items[i - 1].value
-
-        for (j in 1 until  weight) {
-            if(j <= maxWeight)
-                values[j][i] = values[j][i - 1]
-        }
-
-        for (j in weight..maxWeight){
-            values[j][i] = values[j][i - 1].coerceAtLeast(value + values[j - weight][i - 1])
-        }
-    }
-    return values.last().last()
 }
--- Algorithmz/src/com/learn/im/dynamica/Tests.kt
+++ Algorithmz/src/com/learn/im/dynamica/Tests.kt
@@ -12,4 +12,15 @@ fun main(){
     println("ABCABCDABCABCABC".has("xx"))
     println("ABCABCDABCABCABC".has("x"))
 
+
+
+    println(maxBackpackValue(mutableListOf(
+            BackpackItem(4,5),
+            BackpackItem(2,8),
+            BackpackItem(3,3),
+            BackpackItem(4,20),
+            BackpackItem(10,1),
+            BackpackItem(6,30),
+            BackpackItem(1,6)
+    ), 5))
 }
